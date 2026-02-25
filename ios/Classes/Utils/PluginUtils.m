12e3b17594714c61a39440134bd6aa60ce0c15ea
Waleed Ashraf <102048956+waleed750@users.noreply.github.com>
2026-02-25 10:34:14 +0200

    feat: Enhance lockMac retrieval by supporting nested dna and wifi payloads

--- ios/Classes/Utils/PluginUtils.m
+++ ios/Classes/Utils/PluginUtils.m
@@ -21,6 +21,32 @@
 
 + (NSString *)lockMacFromArgs:(NSDictionary *)args {
     NSString *mac = [self stringArg:args key:@"mac"];
+
+    // If not present at top-level, look inside `dna` map (common on iOS calls)
+    if (!mac || mac.length == 0) {
+        id dna = args[@"dna"];
+        if ([dna isKindOfClass:[NSDictionary class]]) {
+            mac = [self stringArg:(NSDictionary *)dna key:@"mac"];
+        }
+    }
+
+    // Also support `wifi` payloads which may contain lockMac or mac
+    if (!mac || mac.length == 0) {
+        id wifi = args[@"wifi"];
+        if ([wifi isKindOfClass:[NSDictionary class]]) {
+            mac = [self stringArg:(NSDictionary *)wifi key:@"lockMac"] ?: [self stringArg:(NSDictionary *)wifi key:@"mac"];
+        } else if ([wifi isKindOfClass:[NSString class]]) {
+            NSData *d = [(NSString *)wifi dataUsingEncoding:NSUTF8StringEncoding];
+            if (d) {
+                NSError *err = nil;
+                id obj = [NSJSONSerialization JSONObjectWithData:d options:0 error:&err];
+                if (!err && [obj isKindOfClass:[NSDictionary class]]) {
+                    mac = [self stringArg:(NSDictionary *)obj key:@"lockMac"] ?: [self stringArg:(NSDictionary *)obj key:@"mac"];
+                }
+            }
+        }
+    }
+
     if (mac.length == 0) return nil;
     return [mac lowercaseString];
 }
